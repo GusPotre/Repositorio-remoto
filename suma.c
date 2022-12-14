0d3fa021294c095c0d8ebff0e095062014059865
GusPotre <120290103+GusPotre@users.noreply.github.com>
2022-12-13 18:27:09 -0600

    suma-resta
    Código de resta en C

--- suma.c
+++ suma.c
@@ -5,8 +5,15 @@ int main()
     int num1 = 3;
     int num2 = 5;
     int suma = 0;
+    
+    int num3 = 4;
+    int num4 = 8;
+    int resta = 0;
 
     suma = num1 + num2;
     printf("%i", suma);
+    
+    resta = num3 - num4;
+    printf("%i", resta);
     return 0;
 }
