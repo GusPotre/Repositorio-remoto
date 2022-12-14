2016158d54da55b07f6d870deee3e54e6a15e8aa
Gus <geuspotrero080103@gmail.com>
2022-12-13 18:17:14 -0600

    Archivos desde el repositorio local

---
+++ suma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 3;
+    int num2 = 5;
+    int suma = 0;
+
+    suma = num1 + num2;
+    printf("%i", suma);
+    return 0;
+}
---
+++ suma.py
@@ -0,0 +1,7 @@
+print("Hola Mundo")
+
+num1 = 3
+num2 = 5
+suma = 0 
+suma = num1+ num2
+print(suma)
